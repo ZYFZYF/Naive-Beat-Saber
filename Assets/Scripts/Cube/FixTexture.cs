74a17e72e1560aec7f70e52e2c6de1dae097e943
ZYFZYF <973446154@qq.com>
2019-12-17 20:55:06 +0800

    增加纹理特殊处理

--- Assets/Scripts/Cube/FixTexture.cs
+++ Assets/Scripts/Cube/FixTexture.cs
@@ -10,28 +10,32 @@ public class FixTexture : MonoBehaviour
     // Use this for initialization
     void Awake()
     {
-        meshFilter = GetComponent<MeshFilter>();
-        Mesh meshcopy = Mesh.Instantiate(meshFilter.sharedMesh) as Mesh;
-        meshcopy.name = "Cube2";
-        mesh = meshFilter.mesh = meshcopy;
-        if (mesh == null || mesh.uv.Length != 24)
-        {
-            Debug.Log("Script needs to be attached to built-in cube");
-            return;
-        }
+        // meshFilter = GetComponent<MeshFilter>();
+        // Mesh meshcopy = Mesh.Instantiate(meshFilter.sharedMesh) as Mesh;
+        // meshcopy.name = "Cube2";
+        // mesh = meshFilter.mesh = meshcopy;
+        // if (mesh == null || mesh.uv.Length != 24)
+        // {
+        //     Debug.Log("Script needs to be attached to built-in cube");
+        //     return;
+        // }
         // uvs = mesh.uv;
-        // uvs[0] = new Vector2(0.0f, 0.0f);
-        // uvs[1] = new Vector2(1.0f, 0.0f);
-        // uvs[2] = new Vector2(0.0f, 0.5f);
-        // uvs[3] = new Vector2(1.0f, 0.5f);
+        // uvs[0] = new Vector2(0.0f, 0.5f);
+        // uvs[1] = new Vector2(0.0f, 0.0f);
+        // uvs[2] = new Vector2(0.5f, 0.5f);
+        // uvs[3] = new Vector2(0.5f, 0.0f);
         // for (int i = 1; i < 6; i++)
         // {
-        //     uvs[i * 4] = new Vector2(0.0f, 0.5f);
-        //     uvs[i * 4 + 1] = new Vector2(1.0f, 0.5f);
-        //     uvs[i * 4 + 2] = new Vector2(0.0f, 1.0f);
-        //     uvs[i * 4 + 3] = new Vector2(1.0f, 1.0f);
+        //     uvs[i * 4 + 0] = new Vector2(0.0f, 0.5f);
+        //     uvs[i * 4 + 1] = new Vector2(0.0f, 0.0f);
+        //     uvs[i * 4 + 2] = new Vector2(0.5f, 0.5f);
+        //     uvs[i * 4 + 3] = new Vector2(0.5f, 0.0f);
+        //     // uvs[i * 4] = new Vector2(0.0f, 0.5f);
+        //     // uvs[i * 4 + 1] = new Vector2(1.0f, 0.5f);
+        //     // uvs[i * 4 + 2] = new Vector2(0.0f, 1.0f);
+        //     // uvs[i * 4 + 3] = new Vector2(1.0f, 1.0f);
         // }
-        mesh.uv = uvs;
+        // mesh.uv = uvs;
     }
     void Start()
     {
@@ -46,41 +50,41 @@ public class FixTexture : MonoBehaviour
         }
         uvs = mesh.uv;
 
-        // // Front
-        // uvs[0] = new Vector2(0.0f, 0.0f);
-        // uvs[1] = new Vector2(0.333f, 0.0f);
-        // uvs[2] = new Vector2(0.0f, 0.333f);
-        // uvs[3] = new Vector2(0.333f, 0.333f);
+        // Front
+        uvs[0] = new Vector2(1.0f, 1.0f);
+        uvs[1] = new Vector2(0.5f, 1.0f);
+        uvs[2] = new Vector2(1.0f, 0.0f);
+        uvs[3] = new Vector2(0.5f, 0.0f);
 
-        // // Top
-        // uvs[8] = new Vector2(0.334f, 0.0f);
-        // uvs[9] = new Vector2(0.666f, 0.0f);
-        // uvs[4] = new Vector2(0.334f, 0.333f);
-        // uvs[5] = new Vector2(0.666f, 0.333f);
+        // Top
+        uvs[8] = new Vector2(1.0f, 1.0f);
+        uvs[9] = new Vector2(0.5f, 1.0f);
+        uvs[4] = new Vector2(1.0f, 0.0f);
+        uvs[5] = new Vector2(0.5f, 0.0f);
 
-        // // Back
-        // uvs[10] = new Vector2(0.667f, 0.0f);
-        // uvs[11] = new Vector2(1.0f, 0.0f);
-        // uvs[6] = new Vector2(0.667f, 0.333f);
-        // uvs[7] = new Vector2(1.0f, 0.333f);
+        // Back
+        uvs[10] = new Vector2(0.5f, 1.0f);
+        uvs[11] = new Vector2(0.0f, 1.0f);
+        uvs[6] = new Vector2(0.5f, 0.0f);
+        uvs[7] = new Vector2(0.0f, 0.0f);
 
-        // // Bottom
-        // uvs[15] = new Vector2(0.0f, 0.333f);
-        // uvs[14] = new Vector2(0.333f, 0.334f);
-        // uvs[12] = new Vector2(0.0f, 0.666f);
-        // uvs[13] = new Vector2(0.333f, 0.666f);
+        // Bottom
+        uvs[15] = new Vector2(1.0f, 1.0f);
+        uvs[14] = new Vector2(0.5f, 1.0f);
+        uvs[12] = new Vector2(1.0f, 0.0f);
+        uvs[13] = new Vector2(0.5f, 0.0f);
 
-        // // Left
-        // uvs[19] = new Vector2(0.334f, 0.334f);
-        // uvs[18] = new Vector2(0.666f, 0.334f);
-        // uvs[16] = new Vector2(0.334f, 0.666f);
-        // uvs[17] = new Vector2(0.666f, 0.666f);
+        // Left
+        uvs[19] = new Vector2(1.0f, 1.0f);
+        uvs[18] = new Vector2(0.5f, 1.0f);
+        uvs[16] = new Vector2(1.0f, 0.0f);
+        uvs[17] = new Vector2(0.5f, 0.0f);
 
-        // // Right
-        // uvs[23] = new Vector2(0.667f, 0.334f);
-        // uvs[22] = new Vector2(1.00f, 0.334f);
-        // uvs[20] = new Vector2(0.667f, 0.666f);
-        // uvs[21] = new Vector2(1.0f, 0.666f);
+        // Right
+        uvs[23] = new Vector2(1.0f, 1.0f);
+        uvs[22] = new Vector2(0.5f, 1.0f);
+        uvs[20] = new Vector2(1.0f, 0.0f);
+        uvs[21] = new Vector2(0.5f, 0.0f);
 
         mesh.uv = uvs;
 
