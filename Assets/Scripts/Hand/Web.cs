6f946f7005cf4b09df3eb15b432dd1f412b2d66e
ZYFZYF <973446154@qq.com>
2019-12-31 15:05:56 +0800

    旋转测试正常

--- Assets/Scripts/Hand/Web.cs
+++ Assets/Scripts/Hand/Web.cs
@@ -138,6 +138,7 @@ public class Web : MonoBehaviour
 
     }
 
+    EndPoint ep;
     private void CallToChildThread()
     {
         Debug.Log("Child thread starts");
@@ -145,11 +146,10 @@ public class Web : MonoBehaviour
         //建立udp服务器，参数2：udp协议以数据报的方式传输，参数3：UDP协议
         Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         //为服务器绑定IP
-        IPAddress ip = IPAddress.Parse("127.0.0.1");
-        EndPoint ep = new IPEndPoint(ip, 2233);
+        ep = new IPEndPoint(IPAddress.Any, 2233);
         udpServer.Bind(ep);
         //接收数据
-        EndPoint endP = new IPEndPoint(IPAddress.Any, 2233);
+        //EndPoint endP = new IPEndPoint(IPAddress.Any, 2233);
         string message;
         byte[] data = new byte[1024];
         int length = 0;
@@ -159,7 +159,7 @@ public class Web : MonoBehaviour
         while (true)
         {
             //Debug.Log("in true");
-            length = udpServer.ReceiveFrom(data, ref endP);
+            length = udpServer.ReceiveFrom(data, ref ep);
             message = Encoding.UTF8.GetString(data, 0, length);
             //Debug.Log (endP.ToString () + message);
             parseString(message);
